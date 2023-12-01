60884ff0e830b7eba399f468d966713dfd58fdd9
Elsan Pentanugraha <elsanpentanugraha687@gmail.com>
2023-12-01 12:17:58 +0700

    fixing Gallery Feature

--- resources/views/global/galeri.blade.php
+++ resources/views/global/galeri.blade.php
@@ -9,15 +9,17 @@
                 <h2 class="text-center">Galeri</h2>
                 <div class="row mt-4">
                     @foreach ($photos as $item)
-                        <div class="col-lg-3 col-md-4 col-sm-6">
-                            <div class="card text-center">
-                                <img src="{{ asset('storage/gallery/' . $item->image) }}" class="card-img-top" alt=""
-                                    onclick="showImage('{{ asset('storage/gallery/' . $item->image) }}', '{{ $item->name }}')">
-                                <div class="card-body">
-                                    <a href="#" class="card-text">{{ $item->name }}</a>
+                        @foreach (explode(',', $item->image) as $imageName)
+                            <div class="col-lg-3 col-md-4 col-sm-6">
+                                <div class="card text-center">
+                                    <img src="{{ asset('storage/gallery/' . $imageName) }}" class="card-img-top" alt=""
+                                        onclick="showImage('{{ asset('storage/gallery/' . $imageName) }}', '{{ $item->name }}')">
+                                    <div class="card-body">
+                                        <a href="#" class="card-text">{{ $item->name }}</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        @endforeach
                     @endforeach
                 </div>
             </div>
